b3afd607373546905a15d4bf579a5fde19bbe7d2
Juan Gutierrez <juan.gutierrez@auro-group.com>
2023-09-23 11:11:12 +0200

    Initial Commit

---
+++ Employees/EmployeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesApp.swift
+//  Employees
+//
+//  Created by Juan Gutierrez on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
